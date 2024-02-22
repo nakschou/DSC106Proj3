d22995af08230cab85646ca1b0af7c6cbabced6d
Charlie-279 <cgillet@ucsd.edu>
2024-02-21 22:32:25 -0800

    switch interface button

--- visualization/src/App.svelte
+++ visualization/src/App.svelte
@@ -1,31 +1,43 @@
 <script>
     import Table from './Table.svelte';
-    // import MapboxMap from './MapboxMap.svelte';
+    import MapboxMap from './MapboxMap.svelte';
+    import Treemap from './Treemap.svelte';
 
-    // import Treemap from './Treemap.svelte';
+    let currentInterface = 'first';
 
-    // // Sample data
-    // let data = [
-    // {id: 'node1', value: 10},
-    // {id: 'node2', value: 15},
-    // // Add more nodes as needed
-    // ];
+    function switchInterface() {
+        currentInterface = currentInterface === 'first' ? 'second' : 'first';
+    }
 
-    // // Function to simulate data updates
-    // function updateData() {
-    // data = [
-    //     // Update with new data
-    //     {id: 'node1', value: Math.random() * 100},
-    //     {id: 'node2', value: Math.random() * 100},
-    //     // More nodes...
-    // ];
-    // }
+    // Sample data
+    let data = [
+        {id: 'node1', value: 10},
+        {id: 'node2', value: 15},
+        // Add more nodes as needed
+    ];
+
+    // Function to simulate data updates
+    function updateData() {
+        data = [
+            // Update with new data
+            {id: 'node1', value: Math.random() * 100},
+            {id: 'node2', value: Math.random() * 100},
+            // More nodes...
+        ];
+    }
 </script>
 
 <Table />
-<!-- <button on:click={updateData}>Update Data</button>
-<Treemap {data} /> -->
-<!-- <MapboxMap /> -->
+<button on:click={updateData}>Update Data</button>
 
+{#key currentInterface}
+    {#if currentInterface === 'first'}
+        <Treemap {data} />
+    {/if}
 
+    {#if currentInterface === 'second'}
+        <MapboxMap />
+    {/if}
+{/key}
 
+<button on:click={switchInterface}>Switch Interface</button>
